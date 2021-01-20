686dcbb12f1fb297b637904ea3fe83ee19fb916d
Giulio <>
2021-01-20 08:04:50 +0100

    update layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -183,7 +183,7 @@
 
 <div class="container-fluid">
 	<div class="row" class:hidden="{!user.is}">
-		<div class="col-sm-8">
+		<div class="col-sm-7">
 			<div><canvas id="chart"></canvas></div>
 		</div>
 		<div class="col" align="center">
