58f9b14962cc9bc84a469d3fcc2fc845c75331d9
Giulio <>
2021-02-22 06:51:30 +0100

    Delay drawing until all datasets are loaded

--- components/counter-chart.vue
+++ components/counter-chart.vue
@@ -35,42 +35,50 @@ mounted() {
 },
 methods: {
     update() {
-        let range = (s, e) => Array.from('x'.repeat(e - s), (_, i) => s + i);
-    
-        let ms_day = 86400000
-        let ms_period = ms_day * {
-            d: 1,
-            w: 7,
-            m: 30,
-            y: 365
-        }[this.period]
-    
-        let colors = [...this.colors]
-        let nextDay = Math.ceil(Date.now() / ms_day) * ms_day
-        let items = 10
-        
-        let data = {
-            datasets: [],
-            labels: range(-items+1, 1),
-        }
-    
-        for (let counter of Object.values(this.counters)) {
-            let dataset = {
-                borderColor: colors.shift(),
-                data: new Array(items).fill(0),
-                fill: false,
-                label: counter.name,
+        globalStartUpdate = Date.now()
+        let localStartUpdate = globalStartUpdate
 
+        setTimeout(() => {
+            // delay drawing until all datasets are loaded
+            if (localStartUpdate == globalStartUpdate) {
+                let range = (s, e) => Array.from('x'.repeat(e - s), (_, i) => s + i);
+            
+                let ms_day = 86400000
+                let ms_period = ms_day * {
+                    d: 1,
+                    w: 7,
+                    m: 30,
+                    y: 365
+                }[this.period]
+            
+                let colors = [...this.colors]
+                let nextDay = Math.ceil(Date.now() / ms_day) * ms_day
+                let items = 10
+                
+                let data = {
+                    datasets: [],
+                    labels: range(-items+1, 1),
+                }
+            
+                for (let counter of Object.values(this.counters)) {
+                    let dataset = {
+                        borderColor: colors.shift(),
+                        data: new Array(items).fill(0),
+                        fill: false,
+                        label: counter.name,
+
+                    }
+                    for (let value of Object.values(counter.values)) {
+                        let index = Math.floor((nextDay-value) / ms_period)
+                        if (index<items)
+                        dataset.data[items-1-index] += 1
+                    }
+                    data.datasets.push(dataset)
+                }
+                this.$chart.data = data
+                this.$chart.update(0)
             }
-            for (let value of Object.values(counter.values)) {
-                let index = Math.floor((nextDay-value) / ms_period)
-                if (index<items)
-                dataset.data[items-1-index] += 1
-            }
-            data.datasets.push(dataset)
-        }
-        this.$chart.data = data
-        this.$chart.update(0)
+        }, 50)
     },
 },
 template:
